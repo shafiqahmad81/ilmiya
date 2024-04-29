1ea523dc8cf3dc1e9028ab5e3247eb4c510ffaad
shafiq <shafiqahmad081@gmail.com>
2024-04-29 19:56:10 +0600

    add icon and image and compleat education section and create ilmiya section

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,14 @@ module.exports = {
         "regal-blue": "#4287F6",
         body: "#0C0C0C",
         "border-1": "#2C2C2C",
+        skyline: "#3F3261",
+        "sky-1": "#4186F3",
+      },
+      fontSize: {
+        "5.5xl": "3.5rem", //56px
+      },
+      lineHeight: {
+        12: "3rem", //48px
       },
     },
   },
