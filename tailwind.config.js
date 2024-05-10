8595e4e101d80c9c4cc038e3c7816c68650333fa
shafiq <shafiqahmad081@gmail.com>
2024-05-10 10:12:52 +0600

    complete ilmiya project

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       },
       spacing: {
         30: "7.5rem", // 120px
+        62: "15.75rem", //252px
       },
       colors: {
         "regal-blue": "#4287F6",
@@ -15,12 +16,20 @@ module.exports = {
         "border-1": "#2C2C2C",
         skyline: "#3F3261",
         "sky-1": "#4186F3",
+        folder: "#F1C159",
       },
       fontSize: {
+        "1.2xl": "1.063rem", //17px
+        "1.5xl": "1.375rem", //22px
+        "4.5xl": "2.5rem", //40px
         "5.5xl": "3.5rem", //56px
       },
       lineHeight: {
         12: "3rem", //48px
+      },
+      borderRadius: {
+        "4xl": "2.25rem", //36px
+        "5xl": "2.5rem", //40px
       },
     },
   },
